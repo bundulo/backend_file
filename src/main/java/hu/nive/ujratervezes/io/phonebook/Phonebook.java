0365bda899f5dab9e14088a6dfabd22eb509911c
mozes <mozes.bence@gmail.com>
2021-04-29 11:40:00 +0200

    almost finished

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,21 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class Phonebook {
+    public void exportPhonebook(Map<String, String> contacts, String output){
+        String eol = System.getProperty("line.separator");
+
+        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(output))) {
+        for (Map.Entry<String, String> entry : contacts.entrySet()){
+            bufferedWriter.append((entry.getKey())).append(": ").append(entry.getValue()).append(eol);
+        }
+
+        } catch (IllegalArgumentException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
